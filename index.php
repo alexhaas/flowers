bbb9b7af5e606d3743379eef9fb227879e2d2cf5
alexhaas <github@alexhaas.me>
2013-07-10 21:17:37 -0400

    Adding the homepage with a basic DOM structure.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>flowers</title>
+	<meta name="language" content="en_US" />
+	<meta http-equiv="content-language" content="en_US" />
+</head>
+<body>
+	<!-- game ui -->
+	<pre>
+		<div>hello world</div>
+	</pre>
+</body>
+</html>
