814b8e25e76c3a736b45d40d78c6175ecff50460
alexhaas <github@alexhaas.me>
2013-07-11 22:01:53 -0400

    Keeping track of item counts with js variables.

--- js/index.js
+++ js/index.js
@@ -4,14 +4,17 @@ $(function() {
 	var $flowers = $('#flowers');
 	var $flowerCount = $('#flowerCount');
 
+	var flowerCount = 0;
+	var preparedFlowersCount = 0;
+
 	var $prepareFlowersButton = $('<button />');
 	$prepareFlowersButton.text('prepare a dozen flowers');
 
 	var $preparedFlowers = $('<div>you have prepared <span id="preparedFlowersCount">0</span> dozen flowers.</div>');
 
 	function incrementFlowers() {
-		var flowerCount = parseInt($flowerCount.text(), 10);
-		$flowerCount.text(flowerCount + 1);
+		flowerCount++;
+		$flowerCount.text(flowerCount);
 		if (flowerCount >= 12) {
 			$prepareFlowersButton.prop('disabled', false);
 			if (!$prepareFlowersButton.is(':visible')) {
@@ -20,15 +23,16 @@ $(function() {
 		}
 	}
 
-	setInterval(incrementFlowers, 500);
+	setInterval(incrementFlowers, 1000);
 
 	$prepareFlowersButton.on('click', function() {
 		if (!$preparedFlowers.is(':visible')) {
 			$game.append($preparedFlowers);
 		}
-		$('#preparedFlowersCount').text(parseInt($('#preparedFlowersCount').text(), 10) + 1);
-		var flowerCount = parseInt($flowerCount.text(), 10);
-		$flowerCount.text(flowerCount - 12);
+		flowerCount -= 12;
+		$flowerCount.text(flowerCount);
+		preparedFlowersCount++;
+		$('#preparedFlowersCount').text(preparedFlowersCount);
 		if (flowerCount < 12) {
 			$prepareFlowersButton.prop('disabled', true);
 		}
