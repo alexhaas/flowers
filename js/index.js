fb4aa5f8f69ee274b093976bf96b03ebe026b931
alexhaas <github@alexhaas.me>
2013-07-11 22:38:54 -0400

    Adding the ability to sell a dozen flowers at a random rate. Starting a piggy bank.

--- js/index.js
+++ js/index.js
@@ -4,17 +4,23 @@ $(function() {
 	var $flowers = $('#flowers');
 	var $flowerCount = $('#flowerCount');
 
+	var piggyBank = 0;
+
 	var flowerCount = 0;
 	var preparedFlowersCount = 0;
+	var preparedFlowersRate = Math.random() * 2.50 + 1.50;
 
-	var $prepareFlowersButton = $('<button />');
-	$prepareFlowersButton.text('prepare a dozen flowers');
-
+	var $prepareFlowersButton = $('<button />').text('prepare a dozen flowers');
 	var $preparedFlowers = $('<div>you have prepared <span id="preparedFlowersCount">0</span> dozen flowers.</div>');
+
+	var $sellPreparedFlowersButton = $('<button />').text('sell a dozen flowers');
+
+	var $piggyBank = $('<div>you have <span id="piggyBankCount">$0.00</span> in your piggy bank.</div>');
 
 	function incrementFlowers() {
 		flowerCount++;
 		$flowerCount.text(flowerCount);
+		// reveal option to prepare flowers
 		if (flowerCount >= 12) {
 			$prepareFlowersButton.prop('disabled', false);
 			if (!$prepareFlowersButton.is(':visible')) {
@@ -22,7 +28,6 @@ $(function() {
 			}
 		}
 	}
-
 	setInterval(incrementFlowers, 1000);
 
 	$prepareFlowersButton.on('click', function() {
@@ -36,6 +41,31 @@ $(function() {
 		if (flowerCount < 12) {
 			$prepareFlowersButton.prop('disabled', true);
 		}
+		// reveal option to sell flowers
+		if (preparedFlowersCount >= 3) {
+			$sellPreparedFlowersButton.prop('disabled', false);
+			if (!$sellPreparedFlowersButton.is(':visible')) {
+				$game.append($sellPreparedFlowersButton);
+			}
+		}
+		if (preparedFlowersCount >= 1) {
+			$sellPreparedFlowersButton.prop('disabled', false);
+		}
+	});
+
+	$sellPreparedFlowersButton.on('click', function() {
+		if (!$piggyBank.is(':visible')) {
+			$game.append($piggyBank);
+		}
+		piggyBank += preparedFlowersRate;
+		$('#piggyBankCount').text($.money(piggyBank));
+		preparedFlowersCount--;
+		$('#preparedFlowersCount').text(preparedFlowersCount);
+		if (preparedFlowersCount < 1) {
+			$sellPreparedFlowersButton.prop('disabled', true);
+		}
+	});
+
 	$.extend({
 		money: function() {
 			return '$' + arguments[0].toFixed(2);
